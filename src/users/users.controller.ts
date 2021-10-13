f463a55dbd2c5c8f62ab0e0c1ee955e50064f17f
Alexander Vasiliev <selado@ya.ru>
2021-10-14 04:08:52 +0500

    Настроен контроллер пользователей

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,42 +1,42 @@
-import { Controller, Param, Body, Redirect, HttpCode, Header, Req, Res, Get, Post, Delete, Put, HttpStatus } from "@nestjs/common";
+import {
+  Controller,
+  Param,
+  Body,
+  HttpCode,
+  Header,
+  Get,
+  Post,
+  Delete,
+  HttpStatus
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { Response, Request } from 'express';
+import { UsersService } from './users.service';
+import { User } from './user.model';
+
 
 @Controller('users')
 export class UsersController {
-
-  // @Get()
-  // // @Redirect('https://ya.ru', 301)
-  // getAll(@Req() req: Request, @Res() res: Response): string {
-  //   res.status(201).end('Chao')
-  //   return 'getAll';
-  // }
+  constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getAll(): string {
-    return 'getAll';
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string): string {
-    return 'getOne ' + id;
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-Control', 'none')
-  create(@Body() createUserDto: CreateUserDto): string {
-    return `Name: ${createUserDto.name}, Surname: ${createUserDto.surname}, Patronymic: ${createUserDto.patronymic}`;
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
   }
 
-  @Delete(":id")
-  remove(@Param('id') id: string) {
-    return 'Remove ' + id;
-  }
-
-  @Put(":id")
-  update(@Body() updateUserDto: UpdateUserDto, @Param('id') id: string) {
-    return 'Update ' + id + `Name: ${updateUserDto.name}, Surname: ${updateUserDto.surname}, Patronymic: ${updateUserDto.patronymic}`
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.usersService.remove(id);
   }
 }
