61f5dc66f7d25cab730bfb39d7d86d39b09285b2
Alexander Vasiliev <selado@ya.ru>
2021-10-13 05:10:11 +0500

    Сделаны наброски работы с sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // SequelizeModule.forRoot({
+    //   dialect: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'knowledgeOld',
+    //   autoLoadModels: true,
+    //   synchronize: true,
+    //   models: [ knowledgeOld ],
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Nest!';
   }
 }
+
+// @Injectable()
+// export class AppService {
+//   constructor(private sequelize: Sequelize) {}
+// }
