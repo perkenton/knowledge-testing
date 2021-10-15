9d8eb76196bc1894b48da4d0af3132c9af88b17e
Alexander Vasiliev <selado@ya.ru>
2021-10-15 22:54:58 +0500

    Созданы комопненты регистрации пользователя

---
+++ src/ui/user-registration/user-registration.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Controller } from "@nestjs/common";
+
+// @Controller('/')
+// export class Page {
+//
+// }
+@Component({
+  selector: 'app-user-registration',
+  templateUrl: './user-registration.component.html',
+
+})
+export class UserRegistrationComponent {}
